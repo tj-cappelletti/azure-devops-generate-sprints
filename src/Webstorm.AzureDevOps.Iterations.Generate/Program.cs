db62bcbc4cdcd2ec3344dc1ea65b2310c21b40ea
TJ Cappelletti <20914729+tj-cappelletti@users.noreply.github.com>
2021-05-31 12:41:23 -0400

    Initial project structure

---
+++ src/Webstorm.AzureDevOps.Iterations.Generate/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Webstorm.AzureDevOps.Iterations.Generate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
